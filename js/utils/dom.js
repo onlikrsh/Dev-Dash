b97b32697d65d611adc0fb401c3117473b716b2e
onlikrsh <kharshith920@gmail.com>
2026-03-02 21:32:02 +0530

    update qsa DOM utility function

--- js/utils/dom.js
+++ js/utils/dom.js
@@ -3,7 +3,7 @@ export function qs(selector){
     return document.querySelector(selector);
 }
 
-export function qs(selector){
+export function qsa(selector){
     return document.querySelectorAll(selector)
 }
 
